89e38c8383e0957528b1187efc52fb02b1e6a4e0
MaratSav <marat1620@gmail.com>
2024-11-21 15:08:38 +0300

    счетчик вызовов

--- module_3_1.py
+++ module_3_1.py
@@ -1,12 +1,23 @@
 calls = 0
+
+
 def count_calls():
     global calls
     calls += 1
 
+
 def string_info(string):
+    count_calls()
+    return len(string), string.upper(), string.lower()
 
 
 def is_contains(string, list_to_search):
+    count_calls()
+    string_lower = string.lower()
+    for elem in list_to_search:
+        if elem.lower() == string_lower:
+            return True
+    return False
 
 
 print(string_info('Capybara'))
