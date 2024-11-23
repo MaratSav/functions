62434b43f216c50cd69309dde227ade806b81f74
MaratSav <marat1620@gmail.com>
2024-11-23 15:20:53 +0300

    рассылка писем

--- module_3_2.py
+++ module_3_2.py
@@ -1,10 +1,15 @@
 def send_email(message, recipient, *, sender='university.help@gmail.com'):
-    if '@' not in recipient or ('.com' not in recipient and '.ru' not in recipient and '.net' not in recipient):
+    domains = ('.com', '.ru', '.net')
+    if '@' not in recipient or not recipient.endswith(domains) or '@' not in sender or not sender.endswith(domains):
         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
         return
     if sender == recipient:
         print(f'Нельзя отправить письмо самому себе!')
         return
+    if sender == 'university.help@gmail.com':
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+    else:
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
 
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
