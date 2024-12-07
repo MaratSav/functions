6367ba73759904ac06ffdbd60f92ad9bb5c95e0b
MaratSav <marat1620@gmail.com>
2024-12-07 15:07:54 +0300

    Это не всё

--- module_3_hard.py
+++ module_3_hard.py
@@ -1,7 +1,6 @@
 data_structure = [[1, 2, 3], {'a': 4, 'b': 5}, (6, {'cube': 7, 'drum': 8}), "Hello",
                   ((), [{(2, 'Urban', ('Urban2', 35))}])
                   ]
-summa = 0
 
 
 def calculate_structure_sum(n):
@@ -16,6 +15,8 @@ def calculate_structure_sum(n):
         elif isinstance(item, str):
             summa += len(item)
     return summa
+
+summa = 0
 
 
 result = calculate_structure_sum(data_structure)
